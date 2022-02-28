a109fe5997896e977f8a1c6274bd719cb764c248
PedroMakengo <61626420+PedroMakengo@users.noreply.github.com>
2022-02-28 18:29:56 -0300

    Criando novas páginas

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
-import { useFetch } from "./hooks/useFetch";
+import axios from "axios";
+import { useQuery } from "react-query";
 
 type Reporitory = {
   full_name: string;
@@ -6,16 +7,27 @@ type Reporitory = {
 };
 
 function App() {
-  const { data: repositories, isFetching } = useFetch<Reporitory[]>(
-    "users/pedromakengo/repos"
+  const { data, isFetching } = useQuery<Reporitory[]>(
+    "repos",
+    async () => {
+      const response = await axios.get(
+        "https://api.github.com/users/pedromakengo/repos"
+      );
+
+      return response.data;
+    },
+    {
+      refetchOnWindowFocus: false,
+    }
   );
+
   return (
     <ul>
       {isFetching && <p>Carregando...</p>}
-      {repositories?.map((repos) => (
-        <li key={repos.full_name}>
-          <strong>{repos.full_name}</strong>
-          <p>{repos.description}</p>
+      {data?.map((repo) => (
+        <li key={repo.full_name}>
+          <strong>{repo.full_name}</strong>
+          <p>{repo.description}</p>
         </li>
       ))}
     </ul>
